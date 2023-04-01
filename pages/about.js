5cf02bc25e8e5e596724fe058eaa10b1eed77a29
Antoine-Toussaint <antoine.toussaint@depinfonancy.net>
2023-04-01 14:11:12 +0200

    fixed href security leaks

--- pages/about.js
+++ pages/about.js
@@ -16,6 +16,7 @@ export default function About() {
           href="https://github.com/jasmin-lang/jasmin/wiki/Installation-instructions"
           className={`${styles.hlink}`}
           target="_blank"
+          rel="noreferrer"
         >
           Wiki - learn the basics{" "}
         </a>
@@ -25,6 +26,7 @@ export default function About() {
           href="https://github.com/jasmin-lang/jasmin"
           className={`${styles.hlink}`}
           target="_blank"
+          rel="noreferrer"
         >
           Jasmin official repository{" "}
         </a>
@@ -34,6 +36,7 @@ export default function About() {
           href="https://dl.acm.org/doi/10.1145/3133956.3134078"
           className={`${styles.hlink}`}
           target="_blank"
+          rel="noreferrer"
         >
           Jasmin paper{" "}
         </a>
@@ -43,6 +46,7 @@ export default function About() {
           href="https://formosa-crypto.gitlab.io/"
           className={`${styles.hlink}`}
           target="_blank"
+          rel="noreferrer"
         >
           Come chat with the Jasmin team here{" "}
         </a>
@@ -53,12 +57,20 @@ export default function About() {
           Who am I ? What is this website ?
         </div>
         <br />I am a french engineering student in{" "}
-        <a href="https://mines-nancy.univ-lorraine.fr/" target="_blank">
+        <a
+          href="https://mines-nancy.univ-lorraine.fr/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Mines Nancy
         </a>
         , and this is the result of a web project. <br />
         If you want to know more about my cool projects, check out my{" "}
-        <a href="https://github.com/Antoine-Toussaint" target="_blank">
+        <a
+          href="https://github.com/Antoine-Toussaint"
+          target="_blank"
+          rel="noreferrer"
+        >
           github
         </a>{" "}
         !
